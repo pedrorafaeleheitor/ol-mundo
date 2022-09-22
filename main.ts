38f23120994b3429b314778cd272c996586640a9
pedrorafaeleheitor <114158044+pedrorafaeleheitor@users.noreply.github.com>
2022-09-22 09:38:29 -0300

    GG

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("Bem-Vindo ao Inferno!")
+    basic.pause(randint(1000, 5000))
+    basic.showIcon(IconNames.Ghost)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+})
 basic.forever(function () {
 	
 })
